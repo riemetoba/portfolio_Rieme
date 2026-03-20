b4d29611b92c494a4e7e8b9e458b5ec3e74e7106
Toba <riemehasan06@gmail.com>
2026-03-20 11:58:26 +0600

    nav

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { IoMdClose } from "react-icons/io";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { FaXTwitter, FaFacebookF } from "react-icons/fa6";
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -18,7 +19,7 @@ const Header = () => {
     <nav className="py-2 lg:py-4 fixed w-screen md:w-full z-10 shadow">
       <div className="container mx-auto flex justify-between items-center px-4">
         <a href="/">
-          <h2 className="font-inter font-medium text-4xl px-2">RIEME</h2>
+          <h2 className="font-poppins font-medium text-4xl px-2">RIEME</h2>
         </a>
 
         {/* Desktop menu */}
@@ -27,13 +28,17 @@ const Header = () => {
             <li key={index}>
               <a
                 href={item.href}
-                className="font-inter text-sm hover:text-orange-500 duration-300 uppercase"
+                className="font-poppins text-sm hover:text-green duration-300 uppercase"
               >
                 {item.name}
               </a>
             </li>
           ))}
         </ul>
+        <div className="hidden sm:flex gap-x-4 items-center text-lg">
+          <FaXTwitter className="hover:text-green duration-300 cursor-pointer" />
+          <FaFacebookF className="hover:text-green duration-300 cursor-pointer" />
+        </div>
 
         {/* Mobile button */}
         <button
@@ -56,13 +61,18 @@ const Header = () => {
               <a
                 href={item.href}
                 onClick={() => setIsOpen(false)}
-                className="font-inter text-sm hover:text-orange-500 duration-300 uppercase"
+                className="font-poppins text-sm hover:text-green duration-300 uppercase"
               >
                 {item.name}
               </a>
             </li>
           ))}
+           <div className="flex gap-x-4 items-center text-lg mt-5">
+          <FaXTwitter className="hover:text-green duration-300 cursor-pointer" />
+          <FaFacebookF className="hover:text-green duration-300 cursor-pointer" />
+        </div>
         </ul>
+        
       </div>
     </nav>
   );
